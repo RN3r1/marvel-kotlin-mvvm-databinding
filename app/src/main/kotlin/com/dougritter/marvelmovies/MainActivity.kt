d1cee4a3e36c4a94425ae8ca9c3d0563facda5cd
Douglas Ritter Machado <drmachado@gmail.com>
2016-03-16 15:18:26 -0300

    Major refactoring to move all logic to viewmodel (MainActivity)

--- app/src/main/kotlin/com/dougritter/marvelmovies/MainActivity.kt
+++ app/src/main/kotlin/com/dougritter/marvelmovies/MainActivity.kt
@@ -5,46 +5,30 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.view.MenuItemCompat
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.SearchView
-import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import com.dougritter.marvelmovies.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
-import rx.Subscription
-import rx.android.schedulers.AndroidSchedulers
-import rx.schedulers.Schedulers
-import rx.subscriptions.CompositeSubscription
-import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ViewModel.MainViewModel.MainActivityViewModel {
 
-    lateinit var originalList: MutableList<Model.Character>
-    val defaultLimit = 20
-    var countLimit = 0
-    lateinit var service: MarvelService
-    private var _compoSub = CompositeSubscription()
-    private val compoSub: CompositeSubscription
-        get() {
-            if (_compoSub.isUnsubscribed) {
-                _compoSub = CompositeSubscription()
-            }
-            return _compoSub
-        }
-
-    protected final fun manageSub(s: Subscription) = compoSub.add(s)
+    lateinit var mainViewModel: ViewModel.MainViewModel
+    lateinit var searchView: SearchView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         var binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        mainViewModel = ViewModel.MainViewModel(this)
+        binding.viewmodel = mainViewModel
         setSupportActionBar(binding.toolbar)
 
-        service = MarvelService.create()
+        mainViewModel.loadCharactersList(this)
+
         val linearLayout = LinearLayoutManager(this)
         recyclerView.layoutManager = linearLayout
-        recyclerView.addOnScrollListener(InfiniteScrollListener({ requestMoreCharacters()}, linearLayout))
+        recyclerView.addOnScrollListener(Utils.InfiniteScrollListener({ mainViewModel.loadMoreCharacters(this, recyclerView.adapter as CharactersAdapter) }, linearLayout))
 
     }
 
@@ -53,132 +37,32 @@ class MainActivity : AppCompatActivity() {
         inflater.inflate(R.menu.menu_activity_main, menu);
 
         val item = menu?.findItem(R.id.search)
-        val searchView = MenuItemCompat.getActionView(item) as SearchView
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextChange(term: String?): Boolean {
-                if (term != null) {
-                    filterList(term)
-                }
-                return false
-            }
-
-            override fun onQueryTextSubmit(term: String?): Boolean {
-                if (term != null) {
-                    filterList(term)
-                }
-                return false
-            }
-
-        });
+        searchView = MenuItemCompat.getActionView(item) as SearchView
 
         return super.onCreateOptionsMenu(menu)
 
     }
 
-    fun filterList(term: String) {
-        if (term != "") {
-            val list = (recyclerView.adapter as CharactersAdapter).characterResponse.data.results.filter { it.name.contains(term.trim(), true) } as MutableList<Model.Character>
-            (recyclerView.adapter as CharactersAdapter).characterResponse.data.results = list
-            (recyclerView.adapter as CharactersAdapter).notifyDataSetChanged()
-
-        } else {
-            (recyclerView.adapter as CharactersAdapter).characterResponse.data.results = originalList
-            (recyclerView.adapter as CharactersAdapter).notifyDataSetChanged()
-        }
-
-    }
-
-
-
     fun charactersList(characterResponse: Model.CharacterResponse) {
         val adapter = CharactersAdapter(characterResponse)
         recyclerView.adapter = adapter
+        searchView.setOnQueryTextListener(mainViewModel.getOnQueryTextChange(recyclerView.adapter as CharactersAdapter));
+
     }
 
-    fun endCallProgress(response: Model.CharacterResponse?) {
-        if (response != null) {
-            charactersList(response)
-            originalList = response.data.results
-            countLimit = response.data.limit
+    override fun endCallProgress(any: Any?) {
+        if (any is Throwable) {
+            println("Error: " + any.message)
+        } else if (any is Model.CharacterResponse) {
+            charactersList(any)
+            println("response "+ any.data.results[0].name)
         }
-    }
-
-    fun endCallMoreProgress(response: Model.CharacterResponse?) {
-        if (response != null) {
-            (recyclerView.adapter as CharactersAdapter).characterResponse = response
-            (recyclerView.adapter as CharactersAdapter).notifyItemRangeChanged(countLimit, countLimit + defaultLimit)
-            originalList = response.data.results
-            countLimit += defaultLimit
-
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        val timestamp = Date().time;
-        val hash = Utils.md5(timestamp.toString()+BuildConfig.MARVEL_PRIVATE_KEY+BuildConfig.MARVEL_PUBLIC_KEY)
-
-        manageSub(
-                service.getCharacters(timestamp.toString(), BuildConfig.MARVEL_PUBLIC_KEY, hash, defaultLimit)
-                        .subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe( { c -> endCallProgress(c)},
-                                { e -> endCallProgress(null)
-                                Log.e(MainActivity::class.java.simpleName, e.message)})
-        )
-
     }
 
     override fun onDestroy() {
-        compoSub.unsubscribe()
+        mainViewModel.unsubscribe()
         super.onDestroy()
     }
 
-    private fun requestMoreCharacters() {
-        val timestamp = Date().time;
-        val hash = Utils.md5(timestamp.toString()+BuildConfig.MARVEL_PRIVATE_KEY+BuildConfig.MARVEL_PUBLIC_KEY)
-
-        manageSub(
-                service.getCharacters(timestamp.toString(), BuildConfig.MARVEL_PUBLIC_KEY, hash, countLimit + defaultLimit)
-                        .subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe( { c -> endCallMoreProgress(c)},
-                                { e -> endCallMoreProgress(null)
-                                    Log.e(MainActivity::class.java.simpleName, e.message)})
-        )
-
-    }
-
-    class InfiniteScrollListener(val func:() -> Unit, val layoutManager: LinearLayoutManager) : RecyclerView.OnScrollListener() {
-        private var previousTotal = 0
-        private var loading = true
-        private var visibleThreshold = 2
-        private var firstVisibleItem = 0
-        private var visibleItemCount = 0
-        private var totalItemCount = 0
-
-        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-            super.onScrolled(recyclerView, dx, dy)
-
-            if (dy > 0) {
-                visibleItemCount = recyclerView.childCount;
-                totalItemCount = layoutManager.itemCount;
-                firstVisibleItem = layoutManager.findFirstVisibleItemPosition();
-
-                if (loading) {
-                    if (totalItemCount > previousTotal) {
-                        loading = false;
-                        previousTotal = totalItemCount;
-                    }
-                }
-                if (!loading && (totalItemCount - visibleItemCount)
-                        <= (firstVisibleItem + visibleThreshold)) {
-                    func()
-                    loading = true;
-                }
-            }
-        }
-
-    }
 
 }
--- app/src/main/kotlin/com/dougritter/marvelmovies/ViewModels.kt
+++ app/src/main/kotlin/com/dougritter/marvelmovies/ViewModels.kt
@@ -3,12 +3,14 @@ package com.dougritter.marvelmovies
 import android.content.Context
 import android.content.Intent
 import android.databinding.*
+import android.support.v7.widget.SearchView
 import android.util.Log
 import android.widget.ImageView
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
 import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
 import rx.Subscription
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
@@ -42,6 +44,99 @@ object ViewModel {
         }
     }
 
+    class MainViewModel(val context: Context) {
+
+        lateinit var originalList: MutableList<Model.Character>
+        val defaultLimit = 20
+        var countLimit = 0
+        lateinit var service: MarvelService
+
+        init {
+            service = MarvelService.create()
+        }
+
+        interface MainActivityViewModel { fun endCallProgress(any: Any?) }
+
+        private var _compoSub = CompositeSubscription()
+        private val compoSub: CompositeSubscription
+            get() {
+                if (_compoSub.isUnsubscribed) {
+                    _compoSub = CompositeSubscription()
+                }
+                return _compoSub
+            }
+
+        protected final fun manageSub(s: Subscription) = compoSub.add(s)
+
+        fun unsubscribe() { compoSub.unsubscribe() }
+
+        fun filterList(term: String, adapter: CharactersAdapter) {
+            if (term != "") {
+                val list = adapter.characterResponse.data.results.filter { it.name.contains(term.trim(), true) } as MutableList<Model.Character>
+                adapter.characterResponse.data.results = list
+                adapter.notifyDataSetChanged()
+
+            } else {
+                adapter.characterResponse.data.results = originalList
+                adapter.notifyDataSetChanged()
+            }
+
+        }
+
+        fun getOnQueryTextChange(adapter: CharactersAdapter) : SearchView.OnQueryTextListener = object : SearchView.OnQueryTextListener{
+            override fun onQueryTextChange(term: String?): Boolean {
+                if (term != null) { filterList(term, adapter) }
+                return false
+            }
+            override fun onQueryTextSubmit(term: String?): Boolean {
+                if (term != null) { filterList(term, adapter) }
+                return false
+            }
+        }
+
+
+        fun loadCharactersList(callback: MainActivityViewModel) {
+
+            val timestamp = Date().time;
+            val hash = Utils.md5(timestamp.toString()+BuildConfig.MARVEL_PRIVATE_KEY+BuildConfig.MARVEL_PUBLIC_KEY)
+
+            manageSub(
+                    service.getCharacters(timestamp.toString(), BuildConfig.MARVEL_PUBLIC_KEY, hash, defaultLimit)
+                            .subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe( { c -> callback.endCallProgress(c)
+                                originalList = c.data.results
+                                countLimit = c.data.limit},
+                                    { e -> callback.endCallProgress(e)
+                                        Log.e(MainActivity::class.java.simpleName, e.message)})
+            )
+        }
+
+        fun loadMoreCharacters(callback: MainActivityViewModel, adapter: CharactersAdapter) {
+            val timestamp = Date().time;
+            val hash = Utils.md5(timestamp.toString()+BuildConfig.MARVEL_PRIVATE_KEY+BuildConfig.MARVEL_PUBLIC_KEY)
+
+            manageSub(
+                    service.getCharacters(timestamp.toString(), BuildConfig.MARVEL_PUBLIC_KEY, hash, countLimit + defaultLimit)
+                            .subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe( { c -> callback.endCallProgress(c)
+                            updateIndexesForRequests(adapter, c)},
+                                    { e -> callback.endCallProgress(e)
+                                        Log.e(MainActivity::class.java.simpleName, e.message)})
+            )
+        }
+
+        public fun updateIndexesForRequests(adapter: CharactersAdapter, response: Model.CharacterResponse) {
+            adapter.characterResponse = response
+            adapter.notifyItemRangeChanged(countLimit, countLimit + defaultLimit)
+            originalList = response.data.results
+            countLimit += defaultLimit
+        }
+
+    }
+
+
     class CharacterDetailViewModel(val context: Context, var intent: Intent) {
 
         lateinit var service: MarvelService
@@ -49,6 +144,7 @@ object ViewModel {
         companion object { val IMAGE_TYPE = "/standard_fantastic." }
 
         init {
+            service = MarvelService.create()
             val characterType = object : TypeToken<Model.Character>() {}.type
             model = Gson().fromJson<Model.Character>(intent.getStringExtra(DetailActivity.MODEL_EXTRA), characterType)
         }
@@ -57,7 +153,7 @@ object ViewModel {
 
         var detailImageUrl = detailImageUrl()
 
-        fun detailImageUrl(): String = model.thumbnail.path + "/standard_fantastic." + model.thumbnail.extension
+        fun detailImageUrl(): String = model.thumbnail.path + IMAGE_TYPE + model.thumbnail.extension
 
         object ImageViewBindingAdapter {
             @BindingAdapter("bind:detailImageUrl")
@@ -81,7 +177,6 @@ object ViewModel {
         fun unsubscribe() { compoSub.unsubscribe() }
 
         fun loadCharacter(callback: DetailViewModel) {
-            service = MarvelService.create()
             val timestamp = Date().time;
             val hash = Utils.md5(timestamp.toString()+BuildConfig.MARVEL_PRIVATE_KEY+BuildConfig.MARVEL_PUBLIC_KEY)
 
